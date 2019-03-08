db6f079f4e567c45aab215f5e89b5caa8f1108e9
ChangYujie <1092622350@qq.com>
2019-03-08 17:41:36 +0800

    实现永久图片视频图文素材的上传

--- wechat-lib/index.js
+++ wechat-lib/index.js
@@ -3,10 +3,22 @@ const request = require("request-promise-native");
 const base = "https://api.weixin.qq.com/cgi-bin/";
 const api = {
   accessToken: base + "token?grant_type=client_credential",
+  //临时素材上传
   temporary: {
     upload: base + "media/upload?"
-  }
+  },
   // https://api.weixin.qq.com/cgi-bin/media/upload?access_token=ACCESS_TOKEN&type=TYPE
+  //永久素材上传
+  permanent: {
+    upload: base + "material/add_material?", //新增其他类型永久素材
+    uploadNews: base + "material/add_news?", //新增永久图文素材
+    uploadNewsPic: base + "media/uploadimg?",//上传图文消息内的图片获取URL
+    fetch: base + "material/get_material?",  //获取素材
+    batch: base + "material/batchget_material?",//获取素材列表
+    count: base + "material/get_materialcount?",//获取素材总数
+    del: base + "material/del_material?",//删除永久素材
+    update: base + "material/update_news?",//修改永久图文素材
+  }
 };
 
 
@@ -17,10 +29,11 @@ class WeChat {
     this.appSecret = opts.appSecret;
     this.getAccessToken = opts.getAccessToken;
     this.saveAccessToken = opts.saveAccessToken;
+    this.isValidToken = this.isValidToken.bind(this);
     // this.fetchAccessToken();
   }
 
-  static async request(options) {
+  async request(options) {
     options = Object.assign({}, options, {json: true});
     try {
       return await request(options);
@@ -34,7 +47,7 @@ class WeChat {
   //3.token入库
   async fetchAccessToken() {
     let data = await this.getAccessToken();
-    if (!WeChat.isValidToken(data)) {
+    if (!this.isValidToken(data)) {
       data = await this.updateAccessToken();
     }
     return data;
@@ -42,39 +55,125 @@ class WeChat {
 
   async updateAccessToken() {
     let url = `${api.accessToken}&appid=${this.appID}&secret=${this.appSecret}`;
-    let data = await WeChat.request({url});
+    let data = await this.request({url});
     data.expires_in = Date.now() + (data.expires_in - 20) * 1000;
     return data;
   }
 
-  static isValidToken(data) {
+  isValidToken(data) {
     if (!data || !data.expires_in) {
       return false;
     }
     return Date.now() < data.expires_in;
   }
 
-  static uploadMaterial(token, type, material, permanent = false) {
+  uploadMaterial(token, type, material, permanent = false) {
     let form = {};
+    //默认是临时素材上传
     let url = api.temporary.upload;
-    //判断是否临时素材
+    //判断是否临时素材 永久素材 form是个Object
     if (permanent) {
-      // url= api.perman
+      url = api.permanent.upload;
+      form = Object.assign(form, permanent);
     }
-    form.media = fs.createReadStream(material);
-    let uploadUrl = `${url}access_token=${token}&type=${type}`;
-    return {
+    //上传图文消息的图片素材
+    if (type === "pic") {
+      url = api.permanent.uploadNewsPic;
+    }
+    // 图文非图文的素材提交表单的切换
+    // 如果是图文的话直接把material交给form表单, 如果不是的话则去读传入文件的路径
+    if (type === "news") {
+      url = api.permanent.uploadNews;
+      form = material;
+    } else {
+      form.media = fs.createReadStream(material);
+    }
+    let uploadUrl = `${url}access_token=${token}`;
+    //根据素材永久性填充token 如果是个form表单则要把token填进去
+    if (!permanent) {
+      uploadUrl += `&type=${type}`;
+    } else {
+      if (type !== "news") {
+        form.access_token = token;
+      }
+    }
+    let options = {
       method: "POST",
       url: uploadUrl,
       json: true,
-      formData: form
     };
+    //图文和非图文在request提交主体判断
+    if (type === "news") {
+      options.body = form;
+    } else {
+      options.formData = form;
+    }
+    return options;
   }
 
   async handle(operation, ...args) {
     let tokenData = await this.fetchAccessToken();
-    let options = WeChat[operation](tokenData.access_token, ...args);
-    return await WeChat.request(options);
+    let options = this[operation](tokenData.access_token, ...args);
+    return await this.request(options);
+  }
+
+  fetchMaterial(token, mediaId, type, permanent) {
+    let form = {};
+    let fetchUrl = api.permanent.fetch;
+    let url = fetchUrl + "access_token=" + token;
+    let options = {
+      method: "POST",
+      url
+    };
+    form.media_id = mediaId;
+    form.access_token = token;
+    options.body = form;
+    return options;
+  };
+
+  deleteMaterial(token, mediaId) {
+    let form = {
+      media_id: mediaId
+    };
+    let url = `${api.permanent.del}access_token=${token}media_id=${mediaId}`;
+    return {
+      method: "POST",
+      url,
+      body: form
+    }
+  }
+
+  updateMaterial(token, mediaId, news) {
+    let form = {
+      media_id: mediaId
+    };
+    form = {...form, ...news};
+    let url = `${api.permanent.update}access_token=${token}media_id=${mediaId}`;
+    return {
+      method: "POST",
+      url,
+      body: form
+    }
+  }
+
+  countMaterial(token) {
+    let url = `${api.permanent.count}access_token=${token}`;
+    return {
+      method: "POST",
+      url,
+    }
+  }
+
+  batchMaterial(token, options) {
+    options.type = options.type || "image";
+    options.offset = options.offset || 0;
+    options.count = options.count || 10;
+    let url = `${api.permanent.batch}access_token=${token}`;
+    return {
+      method: "POST",
+      url,
+      body: options
+    }
   }
 }
 
--- wechat/reply.js
+++ wechat/reply.js
@@ -34,6 +34,127 @@ exports.reply = async (ctx, next) => {
         };
         break;
       }
+      //上传永久视频
+      case "5": {
+        let data = await client.handle(
+          "uploadMaterial",
+          "video",
+          resolve(__dirname, "../6.mp4"),
+          {
+            type: "video",
+            description: JSON.stringify({
+              "title": "VIDEO_TITLE",
+              "introduction": "INTRODUCTION"
+            }),
+          }
+        );
+        console.log(data);
+        reply = {
+          type: "video",
+          title: "回复的视频标题2",
+          description: "打篮球",
+          mediaId: data.media_id
+        };
+        break;
+      }
+      case "6": {
+        let data = await client.handle(
+          "uploadMaterial",
+          "image",
+          resolve(__dirname, "../2.jpg"),
+          {type: "image"}
+        );
+        reply = {
+          type: "image",
+          mediaId: data.media_id
+        };
+        break;
+      }
+      case "7": {
+        let data = await client.handle(
+          "uploadMaterial",
+          "image",
+          resolve(__dirname, "../2.jpg"),
+          {type: "image"}
+        );
+        let data2 = await client.handle(
+          "uploadMaterial",
+          "pic",
+          resolve(__dirname, "../2.jpg"),
+          {type: "image"}
+        );
+        console.log(data2);
+        let media = {
+          articles: [
+            {
+              title: "这是服务端上传的图文1",
+              thumb_media_id: data.media_id,
+              author: "Chang",
+              digest: "这就是摘要",
+              show_cover_pic: 1,
+              content: "点击去往百度",
+              content_source_url: "https://www.baidu.com/",
+              need_open_comment: 1,
+              only_fans_can_comment: 1
+            },
+            {
+              title: "这是服务端上传的图文2",
+              thumb_media_id: data.media_id,
+              author: "Chang",
+              digest: "这就是摘要",
+              show_cover_pic: 1,
+              content: "点击去往github",
+              content_source_url: "https://github.com/",
+              need_open_comment: 1,
+              only_fans_can_comment: 1
+            },
+            //若新增的是多图文素材，则此处应还有几段articles结构
+          ]
+        };
+        let uploadData = await client.handle(
+          "uploadMaterial",
+          "news",
+          media,
+          {}
+        );
+        console.log(uploadData);
+        reply = "上传成功";
+        break;
+      }
+      case "10": {
+        let counts = await client.handle("countMaterial");
+        console.log(JSON.stringify(counts));
+        let res = await Promise.all([
+          client.handle("batchMaterial", {
+            type: "image",
+            offset: 0,
+            count: 10
+          }),
+          client.handle("batchMaterial", {
+            type: "video",
+            offset: 0,
+            count: 10
+          }),
+          client.handle("batchMaterial", {
+            type: "voice",
+            offset: 0,
+            count: 10
+          }),
+          client.handle("batchMaterial", {
+            type: "news",
+            offset: 0,
+            count: 10
+          }),
+        ]);
+        console.log(res);
+        reply = `
+          image: ${res[0].total_count}
+          video: ${res[1].total_count}
+          voice: ${res[2].total_count}
+          news: ${res[3].total_count}
+        `;
+        break;
+      }
       default:
         reply = "听不太懂(⊙o⊙)？";
         break;
